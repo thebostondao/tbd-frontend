793c7f37d00b6ef4378d04beb4905c81977f5c0d
Brian Ford <brianfordp@gmail.com>
2022-03-03 22:16:20 -0500

    strip css

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,17 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
         <title>The Boston DAO</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <div className="">
-          <video autoPlay loop muted>
-            <source src="/intro.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
+      <main>
+        <video autoPlay loop muted>
+          <source src="/intro.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </main>
       
     </div>
